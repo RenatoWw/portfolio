7baa2c720277ca6f00cf9abd01a7f5251c4deb87
RenatoWw <renatoflud@protonmail.com>
2020-02-04 16:20:08 -0300

    Updates

---
+++ precache-manifest.c343da43dfa40b34980686fea815a5f0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4f434ddc85e8dcb7964a1d5236030d",
+    "url": "/renatoww.github.io/index.html"
+  },
+  {
+    "revision": "de05b972f149f0f91744",
+    "url": "/renatoww.github.io/static/css/main.7fe7b19b.chunk.css"
+  },
+  {
+    "revision": "05cf322b7bc8f8ad87ac",
+    "url": "/renatoww.github.io/static/js/2.22d66c0d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/renatoww.github.io/static/js/2.22d66c0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de05b972f149f0f91744",
+    "url": "/renatoww.github.io/static/js/main.270216e7.chunk.js"
+  },
+  {
+    "revision": "e082d9ca40f1c9c0ccb0",
+    "url": "/renatoww.github.io/static/js/runtime-main.5dcc521d.js"
+  },
+  {
+    "revision": "e33767dd76f9f6d337b4a6e795b75f51",
+    "url": "/renatoww.github.io/static/media/background.e33767dd.jpg"
+  },
+  {
+    "revision": "0152ac5c6422770c8d15d4b8f34da4fc",
+    "url": "/renatoww.github.io/static/media/background2.0152ac5c.jpg"
+  },
+  {
+    "revision": "15678ffe04860272db359a247ec7fc08",
+    "url": "/renatoww.github.io/static/media/css3.15678ffe.svg"
+  },
+  {
+    "revision": "e8c27f6b0fcf596a06a31135d937c73e",
+    "url": "/renatoww.github.io/static/media/javascript.e8c27f6b.png"
+  },
+  {
+    "revision": "5b47d4f8d197911857e8e77cb4dd1569",
+    "url": "/renatoww.github.io/static/media/nodejs.5b47d4f8.png"
+  },
+  {
+    "revision": "856e4546b8c202dda03e97771f76f902",
+    "url": "/renatoww.github.io/static/media/python.856e4546.png"
+  },
+  {
+    "revision": "254e2aade1a20e80766f37f209f25845",
+    "url": "/renatoww.github.io/static/media/reactjs.254e2aad.png"
+  },
+  {
+    "revision": "ffb1d0704f59dac3abb78a3dd274b22f",
+    "url": "/renatoww.github.io/static/media/rust.ffb1d070.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c39b4a17f1a8fedc6d8d221358deae2a.js"
+  "/renatoww.github.io/precache-manifest.c343da43dfa40b34980686fea815a5f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/renatoww.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
