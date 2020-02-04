4bab091ff7d1582a4b07ec8153346ef050d59394
RenatoWw <renatoflud@protonmail.com>
2020-02-04 16:08:32 -0300

    Updates

---
+++ precache-manifest.c39b4a17f1a8fedc6d8d221358deae2a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553b0b0498ab2b8880228fd5f8a10ef1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e14b346a599dc8e456b0",
+    "url": "/portfolio/static/css/main.0658c3e1.chunk.css"
+  },
+  {
+    "revision": "05cf322b7bc8f8ad87ac",
+    "url": "/portfolio/static/js/2.22d66c0d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.22d66c0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e14b346a599dc8e456b0",
+    "url": "/portfolio/static/js/main.270216e7.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "e33767dd76f9f6d337b4a6e795b75f51",
+    "url": "/portfolio/static/media/background.e33767dd.jpg"
+  },
+  {
+    "revision": "0152ac5c6422770c8d15d4b8f34da4fc",
+    "url": "/portfolio/static/media/background2.0152ac5c.jpg"
+  },
+  {
+    "revision": "15678ffe04860272db359a247ec7fc08",
+    "url": "/portfolio/static/media/css3.15678ffe.svg"
+  },
+  {
+    "revision": "e8c27f6b0fcf596a06a31135d937c73e",
+    "url": "/portfolio/static/media/javascript.e8c27f6b.png"
+  },
+  {
+    "revision": "5b47d4f8d197911857e8e77cb4dd1569",
+    "url": "/portfolio/static/media/nodejs.5b47d4f8.png"
+  },
+  {
+    "revision": "856e4546b8c202dda03e97771f76f902",
+    "url": "/portfolio/static/media/python.856e4546.png"
+  },
+  {
+    "revision": "254e2aade1a20e80766f37f209f25845",
+    "url": "/portfolio/static/media/reactjs.254e2aad.png"
+  },
+  {
+    "revision": "ffb1d0704f59dac3abb78a3dd274b22f",
+    "url": "/portfolio/static/media/rust.ffb1d070.png"
+  }
+]);
